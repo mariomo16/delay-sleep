186fa1beb6c2dd46a7e3694c36e71a72ca637c23
mariomo16 <mariomo16.cieza@gmail.com>
2026-09-25 19:15:54 +0200

    Initial commit

---
+++ src/client/java/ortega/delaysleep/client/DelaySleepClient.java
@@ -0,0 +1,10 @@
+package ortega.delaysleep.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DelaySleepClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
